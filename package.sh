ed9a0623dea44a89e70c434a793ba7a9d7bc13e3
20nelson <20nelson@da.org>
2019-04-12 20:56:54 -0400

    added packager script

--- package.sh
+++ package.sh
@@ -1,2 +1,4 @@
 cp -vf ./target/minidrive.jar ./dist/minidrive.jar
 cp -vrf ./target/natives ./dist/natives
+version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+zip -r ./minidrive-${version}.zip ./dist
